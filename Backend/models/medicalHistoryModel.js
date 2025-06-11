c4fb4b6aa87bfa4f94b71831f9bedaac298afbb0
Abdulkerim Neja <163136622+abdulkerimN@users.noreply.github.com>
2025-06-11 19:48:08 +0300

    Add files via upload

---
+++ Backend/models/medicalHistoryModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MedicalHistorySchema = new Schema({
+    
+    patientId: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    patientName:{type: String},
+    doctorName:{type: String},
+    doctorId: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    diagnosis: { type: String },
+    treatment: { type: String },
+    notes: { type: String },
+  
+    lastUpdated: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('MedicalHistory', MedicalHistorySchema);
